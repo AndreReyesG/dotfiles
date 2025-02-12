83cce3c9ad722ffc4244ed32a33875aaf41f37e8
AndreReyesG <jandre.reyesg@gmail.com>
2025-02-12 14:27:58 -0600

    feat: add table to change themes more easily

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,8 +7,14 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Tokyo Night Moon"
--- config.color_scheme = "Gruvbox Dark (Gogh)"
+local flavor = {
+  "zenbones_light",
+  "Black Metal (Burzum) (base16)",
+  "kanagawabones",
+  "Tokyo Night Moon",
+}
+
+config.color_scheme = flavor[4]
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 15
