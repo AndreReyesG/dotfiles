ee95b2ce1defe6d47c9239aa64e36342b9a3d468
AndreReyesG <jandre.reyesg@gmail.com>
2025-02-27 09:15:35 -0600

    refactor: colorschemes into modules

--- nvim/.config/nvim/lua/andre/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/andre/plugins/colorscheme.lua
@@ -1,13 +1,5 @@
-return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  enabled = true,
-  config = function()
-    require("catppuccin").setup({
-      flavor = "mocha",
-    })
-    vim.cmd.colorscheme "catppuccin"
-    require("lualine").setup { options = { theme = "catppuccin" } }
-  end
+local colorscheme = {
+  catppuccin = require("andre.colorschemes.catppuccin"),
+  kanagawa = require("andre.colorschemes.kanagawa")
 }
+return colorscheme.kanagawa
