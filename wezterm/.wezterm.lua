693dfd0840005c5f69eeeed5c20a3d768f39b9b6
AndreReyesG <jandre.reyesg@gmail.com>
2025-02-27 09:29:41 -0600

    feat: change theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,16 +8,17 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 local flavor = {
-  "zenbones_light",
-  "Black Metal (Burzum) (base16)",
+  "Kanagawa (Gogh)",
   "kanagawabones",
   "Tokyo Night Moon",
+  "Catppuccin Latte",
+  "Dracula (Official)"
 }
 
-config.color_scheme = flavor[4]
+config.color_scheme = flavor[1]
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 15
+config.font_size = 18
 config.enable_tab_bar = false
 
 -- and finally, return the configuration to wezterm
