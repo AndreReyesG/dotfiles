af297f7977f8e7c0e8df028e31db5f93e6e75c53
AndreReyesG <jandre.reyesg@gmail.com>
2025-03-01 11:34:51 -0600

    feat: add default color wave. wave if the background is dark, otherwise set lotus

--- nvim/.config/nvim/lua/andre/colorschemes/kanagawa.lua
+++ nvim/.config/nvim/lua/andre/colorschemes/kanagawa.lua
@@ -3,6 +3,13 @@ local kanagawa = {
   name = "kanagawa",
   priority = 1000,
   config = function()
+    require("kanagawa").setup({
+      theme = "wave",
+      background = {
+        dark = "wave",
+        light = "lotus",
+      }
+    })
     vim.cmd.colorscheme "kanagawa"
     require("lualine").setup { options = { theme = "kanagawa" } }
   end
