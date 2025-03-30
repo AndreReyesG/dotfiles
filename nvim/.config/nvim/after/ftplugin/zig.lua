9e7a8682fdf12f561d5d8276575cf7b8d5ae4f21
AndreReyesG <jandre.reyesg@gmail.com>
2025-03-30 13:26:40 -0600

    feat(zig.lua): add a keymap to open a small terminal and exit them in zig files

---
+++ nvim/.config/nvim/after/ftplugin/zig.lua
@@ -0,0 +1,13 @@
+local job_id = 0
+vim.keymap.set("n", "<leader>st", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+
+  job_id = vim.bo.channel
+end)
+
+vim.keymap.set("n", "<leader>q", function()
+  vim.fn.chansend(job_id, {"exit\r\n"})
+end)
