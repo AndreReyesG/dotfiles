86fe2c7ba753944ea1a128359c618986b8299285
AndreReyesG <jandre.reyesg@gmail.com>
2025-03-30 13:15:49 -0600

    feat(oil): add oil.nvim plugin

---
+++ nvim/.config/nvim/lua/andre/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+  config = function()
+    require("oil").setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
+
+    vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+  end,
+}
